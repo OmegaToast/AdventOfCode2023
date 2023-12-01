4b651da5b6d0564f5392b7987b85d0eb7480a339
Lucas Speers <lucas_speers@outlook.com>
2023-12-01 12:41:49 -0700

    cleaned part 2

--- 1pt2.py
+++ 1pt2.py
@@ -1,26 +1,18 @@
-file = open('./1.txt', 'r')
-words = file.read().split("\n")
-
+words = open('./1.txt', 'r').read().split("\n")
 numbers = ['one','two','three','four','five','six','seven','eight','nine']
-
 total = 0
 for word in words:
     while True:
         num = None
         lowest_index = 999
         for i,n in enumerate(numbers):
-            try:
+            if n in word:
                 if word.index(n) < lowest_index:
                     num = i
                     lowest_index = word.index(n)
-            except ValueError:
-                pass
-        if num == None:
-            break
-        else:
-            word = word[:lowest_index+1] + str(num+1) + word[lowest_index+1:]
+        if num == None: break
+        else: word = word[:lowest_index+1] + str(num+1) + word[lowest_index+1:]
     first = None
-    last = None
     for letter in word:
         if letter.isnumeric():
             last = letter
